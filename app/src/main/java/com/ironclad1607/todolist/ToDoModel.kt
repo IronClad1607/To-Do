61d074e58034a5bb0b3b43a13652dc8c65c00e6f
Ishaan Jain <ishaan160799@gmail.com>
2020-03-16 18:37:12 +0530

    model file created

---
+++ app/src/main/java/com/ironclad1607/todolist/ToDoModel.kt
@@ -0,0 +1,16 @@
+package com.ironclad1607.todolist
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ToDoModel(
+    var title: String,
+    var description: String,
+    var catergory: String,
+    var date: Long,
+    var time: Long,
+    var isFinished: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+)
